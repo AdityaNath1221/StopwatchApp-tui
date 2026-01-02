80c06d1a3644838535d182dd7cdf757fcce5ef50
AdityaNath1221 <adityanath1221@gmail.com>
2026-01-02 15:09:39 +0530

    Added CSS Styling to app

--- stopwatch.py
+++ stopwatch.py
@@ -1,4 +1,5 @@
 from textual.app import App
+from textual.containers import ScrollableContainer
 from textual.widgets import Header, Footer, Static, Button
 
 class TimeDisplay(Static):
@@ -10,8 +11,8 @@ class Stopwatch(Static):
 
     def compose(self):
         yield Button("Start", id="start", variant="success")
-        yield Button("Stop", id="stop", variant="error")
-        yield Button("Reset", id="reset")
+        yield Button("Stop", id="stop", classes="hidden", variant="error")
+        yield Button("Reset", id="reset", classes="hidden")
         yield TimeDisplay("00:00:00.00")
 
 
@@ -19,13 +20,16 @@ class StopwatchApp(App):
 
     BINDINGS = [("d", "toggle_dark_mode", "Toggle Dark Mode")]
 
+    CSS_PATH = "stopwatch.css"
+
     def compose(self):
         """What widgets are there in the app"""
         yield Header(show_clock=True)
         yield Footer()
-        yield Stopwatch()
-        yield Stopwatch()
-        yield Stopwatch()
+        with ScrollableContainer(id="stopwatches"):
+            yield Stopwatch()
+            yield Stopwatch()
+            yield Stopwatch()
 
     # This is an action method.
     # Defines what a keybinding actually does
