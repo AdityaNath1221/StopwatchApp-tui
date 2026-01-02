e436b122d20212d2f2428b1a9a62231370400506
AdityaNath1221 <adityanath1221@gmail.com>
2026-01-02 21:00:38 +0530

    Added all functionalities of App - Completed

--- stopwatch.py
+++ stopwatch.py
@@ -1,24 +1,83 @@
+from time import monotonic
+
+from textual import on
 from textual.app import App
 from textual.containers import ScrollableContainer
+from textual.reactive import reactive 
 from textual.widgets import Header, Footer, Static, Button
 
 class TimeDisplay(Static):
     """Custom time display widget"""
 
+    accumulated_time = 0
+    start_time = monotonic()
+    time_elapsed = reactive(0)
+
+    def on_mount(self):
+        self.update_timer = self.set_interval(
+            1/60, 
+            self.update_time_elapsed,
+            pause=True,
+        )
+
+    def update_time_elapsed(self):
+        self.time_elapsed = self.accumulated_time + ( monotonic() - self.start_time )
+
+    def watch_time_elapsed(self):
+        time = self.time_elapsed
+        time, seconds = divmod(time, 60)
+        hours, minutes = divmod(time,60)
+        time_string = f"{hours:02.0f}:{minutes:02.0f}:{seconds:05.2f}"
+        self.update(str(time_string))
+
+    def start(self):
+        """Start keeping track of time elapsed"""
+        self.start_time = monotonic()
+        self.update_timer.resume()
+
+    def stop(self):
+        """Stop keeping track of time elapsed"""
+        self.time_elapsed = self.accumulated_time + (monotonic() - self.start_time )
+        self.accumulated_time = self.time_elapsed
+        self.update_timer.pause()
+
+    def reset(self):
+        """Reset time elapsed"""
+        self.accumulated_time = 0
+        self.time_elapsed = 0 
+
 
 class Stopwatch(Static):
     """Custom stopwatch widget"""
 
+    @on(Button.Pressed, "#start") 
+    def start_stopwatch(self):
+        self.add_class("started")
+        self.query_one(TimeDisplay).start()
+
+    @on(Button.Pressed, "#stop")
+    def stop_stopwatch(self):
+        self.remove_class("started")
+        self.query_one(TimeDisplay).stop()
+
+    @on(Button.Pressed , "#reset")
+    def reset_stopwatch(self):
+        self.query_one(TimeDisplay).reset()
+
     def compose(self):
         yield Button("Start", id="start", variant="success")
         yield Button("Stop", id="stop", classes="hidden", variant="error")
-        yield Button("Reset", id="reset", classes="hidden")
+        yield Button("Reset", id="reset")
         yield TimeDisplay("00:00:00.00")
 
 
 class StopwatchApp(App):
 
-    BINDINGS = [("d", "toggle_dark_mode", "Toggle Dark Mode")]
+    BINDINGS = [
+        ("D", "toggle_dark_mode", "Toggle Dark Mode"),
+        ("A", "add_stopwatch", "Add"),
+        ("R", "remove_stopwatch", "Remove"),
+    ]
 
     CSS_PATH = "stopwatch.css"
 
@@ -28,13 +87,22 @@ class StopwatchApp(App):
         yield Footer()
         with ScrollableContainer(id="stopwatches"):
             yield Stopwatch()
-            yield Stopwatch()
-            yield Stopwatch()
 
     # This is an action method.
     # Defines what a keybinding actually does
     def action_toggle_dark_mode(self):
         self.theme="textual-dark" if self.theme=="textual-light" else "textual-light"
+
+    def action_add_stopwatch(self):
+        stopwatch = Stopwatch()
+        container = self.query_one("#stopwatches")
+        container.mount(stopwatch)
+        stopwatch.scroll_visible()
+
+    def action_remove_stopwatch(self):
+        stopwatches = self.query(Stopwatch)
+        if stopwatches:
+            stopwatches.last().remove()
     
 
 if __name__ == "__main__":
